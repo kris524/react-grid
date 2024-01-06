d2a6c8899858b8d843ed5d1f8edde16b1958a498
Kris <kristian_dilov@abv.bg>
2024-01-06 19:14:34 +0200

    add grid

---
+++ src/components/grid.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import GridLayout from "react-grid-layout";
+export const Grid = ({gridDim}) => {
+
+  console.log(gridDim)
+
+//   const createData = () => {
+    
+
+//     }
+  
+  return (
+    <div className='grid grid-cols-4 gap-4'>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+
+
+    </div>
+
+  )
+}
---
+++ src/components/input.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Grid } from './grid'
+
+export const InputForm = () => {
+    const [gridDim, setGridDim] = useState(0)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setGridDim(event.target.grid.value)
+
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label className='flex flex-col'>Grid size</label>
+            <input id="grid" placeholder="Enter a number" type="number" name='grid' min="1" max="100" className="bg-gray-50 border mb-3 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <button type="submit" className="bg-blue-500 mb-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"  >Create Grid</button>
+        </form>
+        <Grid gridDim={gridDim}></Grid>
+    </div>
+  )
+}
+
+
+
