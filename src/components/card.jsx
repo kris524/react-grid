4bbaa5e201316ab3d6689cebc7d713ac68d87672
Kris <kristian_dilov@abv.bg>
2024-01-07 13:56:32 +0200

    add card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Card = () => {
+export const Card = ({number}) => {
+
+    const [color, setColor] = useState("black")
+
+
   return (
-    <div>card</div>
+    <div style={{color: color}} className='block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
+        <h2 className='font-bold text-xl mb-2'>{number}</h2>
+    </div>
   )
+  
 }
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import GridLayout from "react-grid-layout";
+import { Card } from './card';
 export const Grid = ({gridDim}) => {
 
   console.log(gridDim)
 
-//   const createData = () => {
-    
-
-//     }
-  
+  let number = 1;
   return (
     <div className='grid grid-cols-4 gap-4'>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+        <Card number={number}></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
 
 
     </div>
