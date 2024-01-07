83c1c864b12c84e78936eb88ad0ca1fe66aa379c
Kris <kristian_dilov@abv.bg>
2024-01-07 17:54:10 +0200

    fix card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 
 export const Card = ({number}) => {
 
-    const [color, setColor] = useState("black")
+    const [color, setColor] = useState("white")
 
 
   return (
-    <div style={{color: color}} className='block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
+    <div onClick={() => {
+        setColor(color === "white" ? "#1ad1ff" : "white")
+        }} style={{background: color}} className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h2 className='font-bold text-xl mb-2'>{number}</h2>
     </div>
   )
