a3fcffa30c0471df89ace646b05db68b11f1977b
Kris <kristian_dilov@abv.bg>
2024-01-20 21:52:20 +0000

    render the matrix

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
 import GridLayout from "react-grid-layout";
 import { Card } from './card';
+
+
+function createMatrix(size) {
+
+    const matrix = []
+
+    let start = 1
+
+    for(let i=0; i<size; i++) {
+        let row = []
+        for (let j=0; j<size; j++){
+            let num_in_box = start**2
+            row.push(<Card number={num_in_box}></Card>)
+            start+=1
+        }
+        matrix.push(row)
+
+    }
+    // console.log("Matrix is:", matrix)
+    return matrix
+
+}
+
 export const Grid = ({gridDim}) => {
 
-  console.log(gridDim)
+  let matrix = createMatrix(gridDim)
 
-  let number = 1;
   return (
-    <div className='grid grid-cols-4 gap-4'>
-        <Card number={number}></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-
-
-    </div>
+        <table>
+        <tbody>
+        {matrix.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+            {row.map((cell, colIndex) => (
+                <td key={colIndex}>{cell}</td>
+            ))}
+            </tr>
+        ))}
+        </tbody>
+    </table>
 
   )
 }
